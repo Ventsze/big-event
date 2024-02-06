dbfe269a1cf94bcd585999a6afe3ac0223ed5a58
章文泽 <zwz614371723@icloud.com>
2024-02-06 18:28:32 +0300

    练习SpringBoot项目

--- src/main/java/com/ventsze/controller/UserController.java
+++ src/main/java/com/ventsze/controller/UserController.java
@@ -3,34 +3,34 @@ package com.ventsze.controller;
 import com.ventsze.pojo.Result;
 import com.ventsze.pojo.User;
 import com.ventsze.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public Result register(String username,String password){
-
+    public Result register(@Pattern(regexp = "^\\S{5,16}$")String username,@Pattern(regexp = "^\\S{5,16}$")String password){
         //查询用户
         User u = userService.findByUserName(username);
         if( u == null ){
             //用户名没有占用
+            //注册
             userService.register(username,password);
             return Result.success();
         }else {
             //用户名被占用
             return Result.error("用户名已被占用");
         }
-
-        //注册
-
     }
-
 }
+
