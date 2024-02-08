9c97948bfb56639243ec10f188b63e19aeddfdd9
章文泽 <zwz614371723@icloud.com>
2024-02-08 12:55:53 +0300

    给用户基本信息添加参数校验，防止不合法。

--- src/main/java/com/ventsze/pojo/User.java
+++ src/main/java/com/ventsze/pojo/User.java
@@ -3,7 +3,12 @@ package com.ventsze.pojo;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import lombok.NonNull;
 
 import java.time.LocalDateTime;
 
@@ -11,11 +16,20 @@ import java.time.LocalDateTime;
 // 要使用lombok，需要在pom文件中引入依赖 ，在实体类上添加注解
 @Data
 public class User {
+    @NotNull //值不能为Null
     private Integer id;//主键ID
+
     private String username;//用户名
+
     @JsonIgnore //让springmvc把当前对象转换成json字符串的时候，忽略password，最终json字符串就没有password属性了。
     private String password;//密码
+
+    @NotEmpty //值不能为Null，且内容不能为空
+    @Pattern(regexp = "^\\S{1,10}$")
     private String nickname;//昵称
+
+    @NotEmpty
+    @Email
     private String email;//邮箱
     private String userPic;//用户头像地址
     private LocalDateTime createTime;//创建时间
