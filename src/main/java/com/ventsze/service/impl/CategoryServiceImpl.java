2c33b394d0017f39e925d9590c4935ac64dbaeba
章文泽 <zwz614371723@icloud.com>
2024-02-10 13:14:53 +0300

    新增文章分类接口

---
+++ src/main/java/com/ventsze/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,30 @@
+package com.ventsze.service.impl;
+
+import com.ventsze.mapper.CategoryMapper;
+import com.ventsze.pojo.Category;
+import com.ventsze.service.CategoryService;
+import com.ventsze.utils.ThreadLocalUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    @Override
+    public void add(Category category) {
+        //补充属性值
+        category.setCreateTime(LocalDateTime.now());
+        category.setUpdateTime(LocalDateTime.now());
+
+        Map<String,Object> map = ThreadLocalUtil.get();
+        Integer userId = (Integer) map.get("id");
+        category.setCreateUser(userId);
+        categoryMapper.add(category);
+    }
+}
